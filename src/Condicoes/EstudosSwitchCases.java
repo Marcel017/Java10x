5ebf5dc583844b3530ef9bc2e59034f36867bb07
Marcel017 <mcelogabriel17@gmail.com>
2026-01-04 16:22:28 -0300

    Aprendendo um pouco mais sobre Switchcases, aprendi que ela e usada quando vamos aplicar uma condição especifica
    switch(nome do caso) {
     case 1 (o caso)
     default ( para parar a plicação caso o usuario Não va para o caminho certo)
    }

---
+++ src/Condicoes/EstudosSwitchCases.java
@@ -0,0 +1,49 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class EstudosSwitchCases {
+    public static void main(String[] args) {
+        /*
+        SwitchCases: Que servem para gerar casos especificos
+        Objetivo: Pedir pro usuario escolher entre os ninjas
+        switchcase
+         */
+
+        //Pedir para o usuario
+        Scanner scanner = new Scanner(System.in);
+
+
+        //Mostrar opcoes para o usuario
+        System.out.println("Escolha um personagem: ");
+        System.out.println("1 - Naruto Uzumaki");
+        System.out.println("2 - Sasuke Uchiha");
+        System.out.println("3 - Sakura Haruno");
+
+        //Pedir para o usuario escolher uma das opcoes
+        int escolhaDoUsuario = scanner.nextInt();
+
+        System.out.println("Voce digitou o numero: " + escolhaDoUsuario);
+
+        //Reacao ao escolher um personagem
+
+        switch (escolhaDoUsuario) {
+            case 1:
+                System.out.println("O usuario escolheu o Naruto Uzumaki");
+                break;
+            case 2:
+                System.out.println("O usuario escolheu o Sasuke, o ninja mais revoltado");
+                break;
+            case 3:
+                System.out.println("O usuario escolheu a Sakura Haruno, a personagem mais legal");
+                break;
+            default:
+                System.out.println("Voce nao digitou uma resposta valida! tente denovo");
+                break;
+        }
+
+        //Fechar a caixa
+        scanner.close();
+
+    }
+}
