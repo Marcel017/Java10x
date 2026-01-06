10b0fe49c7b070500f311e4d84b94142fa5a80cc
Marcel017 <mcelogabriel17@gmail.com>
2026-01-05 22:38:40 -0300

    Aqui entendendo um pouco mais sobre Array, literamente um jeito de agrupar o codigo, aprendendo sobre index, a contagem sempre começa em zero, a criação de um array seria assim String[] exemplo = new String[Espaço a ser alocado];

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[4];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        ninja[3] = "Hinata Hyuga";
+
+        System.out.println(ninja[1]);
+
+//
+//        String ninja1 = "Naruto Uzumaki";
+//        String ninja2 = "Sasuke Uchiha";
+//        String ninja3 = "Sakura Haruno";
+//
+//        System.out.println("Naruto Uzumaki");
+//        System.out.println("Sasuke Uchiha");
+//        System.out.println("Sakura Haruno");
+//
+    }
+}
